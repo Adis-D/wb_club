4add0ab17495b874765a2cedceffba633beb6e50
Adis-D <adis.com.work@gmail.com>
2024-10-16 18:52:28 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// document.querySelectorAll('.accordion-header').forEach(header => {
+//     header.addEventListener('click', () => {
+//         const content = header.nextElementSibling;
+//         const isOpen = content.classList.toggle('open');
+
+//         // Close other sections
+//         document.querySelectorAll('.accordion-content').forEach(otherContent => {
+//             if (otherContent !== content && otherContent.classList.contains('open')) {
+//                 otherContent.classList.remove('open');
+//             }
+//         });
+//     });
+// });
